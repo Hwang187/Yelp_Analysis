0e07dcd560cd83d68b2fd3f6023ca0046c4600dd
Hwang187 <42648850+Hwang187@users.noreply.github.com>
2019-05-04 16:35:12 -0500

    Update CitySpecify.R

--- CitySpecify.R
+++ CitySpecify.R
@@ -48,18 +48,5 @@ restaurantSpecify = function(businessLVc)
   return("Output successful")
 }
 
-# Checking missing value and make visualization
-missingMap <- function(businessLVc)
-{
-  # Check missing value
-  Null_Counter = apply(businessLVc, 2, function(x) length(which(x == "" | is.na(x) | x == "NA"))/length(x))
-  Null_Percent = Null_Counter[which(Null_Counter > 0)]
-  barPlot = barplot(sort(Null_Percent), las=2, 
-                    col=rainbow(49), cex.names=.5, ylim=c(0,1),
-                    main = 'Missing Value Percentage',
-                    ylab = 'Missing Percentage')
-  return(barPlot)
-}
-
 
 
